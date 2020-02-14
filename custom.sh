da1946d1785507b9181da7bcfc9808872f5e53ec
D-Bood <dbood2012@gmail.com>
2020-02-14 09:05:29 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -7,5 +7,6 @@ opkg install luci-theme-darkmatter_0.2-beta-2_all.ipk --force-reinstall
 
 cp /www/luci-static/material/fonts/font.woff /www/luci-static/darkmatter/fonts/DIN.woff
 
-sed -i 's/background-color: #fff;/background-color: #2f2e2a;/g' /usr/lib/lua/luci/view/passwall/global/status.htm
+sed -i 's/background-color: #fff;/background-color: #3f3e3a;/g' /usr/lib/lua/luci/view/passwall/global/status.htm
 sed -i 's/os.date()/os.date("%Y年%m月%d日 %H:%M:%S")/g' /usr/lib/lua/luci/view/admin_status/index.htm
+sed -i 's/max-width:200px;//g' /usr/lib/lua/luci/view/admin_status/index.htm
